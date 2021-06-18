f549df802e76b1eadbc655e219021afa3ba8df15
“Haorong <li000592@algonquinlive.com>
2021-06-18 14:29:54 -0400

    :sparkles: set the basic environment for Reactfire

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useFirestoreDocData, useFirestore } from "reactfire"
+import "firebase/firestore"
+
+interface Data {
+  yummy: any
+}
+function Burrito() {
+  // easily access the Firestore library
+  const burritoRef = useFirestore().collection("tryreactfire").doc("burrito")
+  //const reviewPostRef = useFirestore().collection("reviewposts").doc("post")
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData<Data>(burritoRef)
+  //const { status, data } = useFirestoreDocData<Data>(reviewPostRef)
+  console.log(data)
+
+  // easily check the loading status
+  if (status === "loading") {
+    return <p>Fetching burrito flavor...</p>
+  }
+
+  return <p>The burrito is {data.yummy ? "good" : "bad"}!</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>🌯</h1>
+      <Burrito />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { FirebaseAppProvider } from "reactfire"
+
+const firebaseConfig = {
+  /* Add your config from the Firebase Console */
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGAINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+}
+console.log(process.env)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
